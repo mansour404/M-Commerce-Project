301432035a783c7a8cd1f756decacb471250c74c
mansour404 <mansour5.dev@gmail.com>
2023-10-19 15:38:43 +0300

    Start Payment UI

---
+++ E-Commerce-Project/Screens/Order/View/PaymentViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PaymentViewController.swift
+//  E-Commerce-Project
+//
+//  Created by Mohammed Adel on 19/10/2023.
+//
+
+import UIKit
+
+class PaymentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
