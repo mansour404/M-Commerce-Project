b2d125321ea28c3f16bafb010862360493512809
mansour404 <mansour5.dev@gmail.com>
2023-10-23 06:58:41 +0300

    payment view done

---
+++ E-Commerce-Project/Screens/Payment/View/PaymentView.swift
@@ -0,0 +1,129 @@
+//
+//  PaymentView.swift
+//  E-Commerce-Project
+//
+//  Created by Mohammed Adel on 19/10/2023.
+//
+
+import UIKit
+import PassKit
+
+@objc enum PaymentState: Int {
+    case selected = 1
+    case notSelected = 0
+}
+
+class PaymentView: UIViewController {
+    
+    // MARK: - Vars
+    var isHiddenFlag: Bool = true
+    var cashState: PaymentState = .notSelected
+    var creditState: PaymentState = .notSelected
+    
+
+    // MARK: - Outlets
+    @IBOutlet weak var creditCardView: UIView!
+    @IBOutlet weak var cashOnDeliveryView: UIView!
+    @IBOutlet weak var creditSelectedImageView: UIImageView!
+    @IBOutlet weak var cashSelectedImageView: UIImageView!
+    
+    @IBOutlet weak var orderInfoView: UIView!
+    @IBOutlet weak var subtotalLabel: UILabel!
+    @IBOutlet weak var shippingCostLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var checkoutButton: UIButton!
+    
+    
+    // MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        setupGestureRecognizer()
+    }
+    
+    @IBAction func checkoutButtonPressed(_ sender: Any) {
+        // TODO: - Present view controller of apple pay at the bottom of the view controller
+    }
+    
+    // MARK: - Configure UI
+    private func configureUI() {
+        checkoutButton.addCornerRadius()
+        checkoutButton.isEnabled = false
+        creditSelectedImageView.isHidden = true
+        cashSelectedImageView.isHidden = true
+        configureCardView(creditCardView)
+        configureCardView(cashOnDeliveryView)
+    }
+    
+    private func configureCardView(_ view: UIView) {
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        view.dropShadow()
+    }
+    
+    // MARK: - Setup gesture recognizer
+    func setupGestureRecognizer() {
+        let creditGesture = UITapGestureRecognizer(target: self, action: #selector(getFiredCredit(_:)))
+        let cashGesture = UITapGestureRecognizer(target: self, action: #selector(getFiredCash(_:)))
+        
+//        let creditGesture = UITapGestureRecognizer(target: self, action: #selector(underTest(_, firstState: &creditState, secondState: &cashState, firstView: creditSelectedImageView, secondView: cashSelectedImageView)))
+//        let cashGesture = UITapGestureRecognizer(target: self, action: #selector(underTest(_, firstState: &cashState, secondState: &creditState, firstView: cashSelectedImageView, secondView: creditSelectedImageView)))
+
+        addGesture(view: creditCardView, gesture: creditGesture)
+        addGesture(view: cashOnDeliveryView, gesture: cashGesture)
+    }
+    
+    private func addGesture(view: UIView, gesture:UITapGestureRecognizer) {
+        view.addGestureRecognizer(gesture)
+        view.isUserInteractionEnabled = true
+    }
+
+    
+//    @objc func underTest(_ sender: UITapGestureRecognizer? = nil, firstState: UnsafeMutablePointer<PaymentState>, secondState: UnsafeMutablePointer<PaymentState>, firstView: UIView, secondView: UIView) {
+//
+//        switch firstState.pointee {
+//        case .selected:
+//            firstView.isHidden = true
+//            checkoutButton.isEnabled = false
+//            firstState.pointee = .notSelected
+//        case .notSelected:
+//            firstView.isHidden = false
+//            checkoutButton.isEnabled = true
+//            firstState.pointee = .selected
+//        }
+//        secondView.isHidden = true
+//        secondState.pointee = .notSelected
+//    }
+
+    @objc func getFiredCredit(_ sender: UITapGestureRecognizer) {
+        switch creditState {
+        case .selected:
+            creditSelectedImageView.isHidden = true
+            checkoutButton.isEnabled = false
+            creditState = .notSelected
+        case .notSelected:
+            creditSelectedImageView.isHidden = false
+            checkoutButton.isEnabled = true
+            creditState = .selected
+        }
+        cashSelectedImageView.isHidden = true
+        cashState = .notSelected
+    }
+    
+    @objc func getFiredCash(_ sender: UITapGestureRecognizer) {
+        switch cashState {
+        case .selected:
+            cashSelectedImageView.isHidden = true
+            checkoutButton.isEnabled = false
+            cashState = .notSelected
+        case .notSelected:
+            cashSelectedImageView.isHidden = false
+            checkoutButton.isEnabled = true
+            cashState = .selected
+        }
+        creditSelectedImageView.isHidden = true
+        creditState = .notSelected
+    }
+    
+}
