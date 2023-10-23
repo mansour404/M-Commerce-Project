05878cb619ac441623d269c3c4142f6541f3f79a
Ziyad-Qassem <Qassemziyad007@gmail.com>
2023-10-23 09:52:32 +0300

    favourite and auth UI added

---
+++ E-Commerce-Project/Screens/Favourites/FavouriteCell.swift
@@ -0,0 +1,20 @@
+//
+//  FavouriteCell.swift
+//  E-Commerce-Project
+//
+//  Created by Ziyad Qassem on 20/10/2023.
+//
+
+import UIKit
+
+class FavouriteCell: UICollectionViewCell {
+
+    @IBOutlet weak var FavouriteProductPrice: UILabel!
+    @IBOutlet weak var FavouriteProductName: UILabel!
+    @IBOutlet weak var FavouriteProductImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ E-Commerce-Project/Screens/Login and SignUp/Login_or_Singup.swift
@@ -0,0 +1,29 @@
+//
+//  Login_or_Singup.swift
+//  finalprojectmock
+//
+//  Created by Ziyad Qassem on 18/10/2023.
+//
+
+import UIKit
+
+class Login_or_Singup: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
